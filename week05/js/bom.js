7305e5f752b8e30cc1130984819c94f8d8de1ec8
spensnerd <griffinfspencer@gmail.com>
2022-02-03 22:27:41 -0600

    bom assignment

---
+++ week05/js/bom.js
@@ -0,0 +1,7 @@
+const listsize = document.querySelector('#favchap');
+const buildButton = document.querySelector('button');
+const list = document.querySelector('#list');
+
+buildButton.addEventListener('click', function() {
+let size = Number(listsize.value);
+})
