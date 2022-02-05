939e370c947259d670e8974b63bf530f97c00b3e
spensnerd <griffinfspencer@gmail.com>
2022-02-05 10:25:46 -0600

    Week 04

--- week05/js/bom.js
+++ week05/js/bom.js
@@ -4,4 +4,10 @@ const list = document.querySelector('#list');
 
 buildButton.addEventListener('click', function() {
 let size = Number(listsize.value);
+
+for (let i = 1; i <= size; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = `Item: <strong>${i}</strong>`;
+    list.appendChild(li);
+}
 })
