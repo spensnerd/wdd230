5d6c98a76fb0113d78b59ee03162560716982a15
spensnerd <griffinfspencer@gmail.com>
2022-05-18 21:03:49 -0500

    BoM project

--- week5/js/bom.js
+++ week5/js/bom.js
@@ -1,13 +1,38 @@
-const listsize = document.querySelector('#favchap');
-const buildButton = document.querySelector('button');
-const list = document.querySelector('#list');
+// const listsize = document.querySelector('#favchap');
+// const buildButton = document.querySelector('button');
+// const list = document.querySelector('#list');
 
-buildButton.addEventListener('click', function() {
-let size = Number(listsize.value);
+// buildButton.addEventListener('click', function() {
+// let size = Number(listsize.value);
 
-for (let i = 1; i <= size; i++) {
-    let li = document.createElement('li');
-    li.innerHTML = `Item: <strong>${i}</strong>`;
-    list.appendChild(li);
-}
-})
+// for (let i = 1; i <= size; i++) {
+//     let li = document.createElement('li');
+//     li.innerHTML = `Item: <strong>${i}</strong>`;
+//     list.appendChild(li);
+// }
+// });
+
+const list = document.querySelector('ul');
+      const input = document.querySelector('input');
+      const button = document.querySelector('button');
+
+      button.addEventListener('click', () => {
+        const myItem = input.value;
+        input.value = '';
+
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
+
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = '❌';
+        list.appendChild(listItem);
+
+        listBtn.addEventListener('click', () => {
+          list.removeChild(listItem);
+        });
+
+        input.focus();
+      });
