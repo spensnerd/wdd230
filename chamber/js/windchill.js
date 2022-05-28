a62243f76c1aa25895c740684f3015d1975665a5
spensnerd <griffinfspencer@gmail.com>
2022-05-28 18:26:54 -0500

    Kind of working?

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -3,13 +3,25 @@ f = wind chill factor
 t = average air temperature
 s = wind speed in miles per hour
 */
-function findWindChill() {
-  var wind = 5; /*document.getElementById("windSpeed").textContent*/
-  var temperature = 35; /*document.getElementById("highTemp").textContent*/
+
+let t = parseFloat(document.querySelector("#t").textContent);
+let s = parseFloat(document.querySelector("#s").textContent);
+let windchill = "";
+if (t <= 50 && s > 3) {
+  windchill = windChill(t, s);
+  windchill = `${windchill}&#176;F`;
+} else {
+  windchill = "N/A";
+}
+
+document.querySelector("#w").innerHTML = windchill;
+
+function windChill(temp, speed) {
+
   var windChill =
     (35.74 +
-    (0.6215 * temperature)) -
-    (35.75 * Math.pow(wind,0.16)) +
-    (0.4275 * temperature * Math.pow(wind,0.16));
-  console.log(Math.round(windChill * 10) / 10);
+    (0.6215 * temp)) -
+    (35.75 * Math.pow(speed,0.16)) +
+    (0.4275 * temp * Math.pow(speed,0.16));
+  return(Math.round(windChill * 10) / 10);
 }
