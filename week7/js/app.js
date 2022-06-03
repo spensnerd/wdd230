d50576f6498bcea4f4f6dc92060897fe43ecb3c2
spensnerd <griffinfspencer@gmail.com>
2022-06-02 20:24:06 -0500

    a few tweaks

--- week7/js/app.js
+++ week7/js/app.js
@@ -1,30 +1,62 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+// let imagesToLoad = document.querySelectorAll('img[data-src]');
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
+// const loadImages = (image) => {
+//     image.setAttribute('src', image.getAttribute('data-src'));
+//     image.onload = () => {
+//         image.removeAttribute('data-src');
+//     };
+// };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+// });
 
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        itemns.forEach((item) => {
-            if(item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-    });
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
-    });
-} else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
+// if('IntersectionObserver' in window) {
+//     const observer = new IntersectionObserver((items, observer) => {
+//         itemns.forEach((item) => {
+//             if(item.isIntersecting) {
+//                 loadImages(item.target);
+//                 observer.unobserve(item.target);
+//             }
+//         });
+//     });
+//     imagesToLoad.forEach((img) => {
+//         observer.observe(img);
+//     });
+// } else {
+//     imagesToLoad.forEach((img) => {
+//         loadImages(img);
+//     });
+// }
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
 }
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+const imgObserver = new IntersectionObserver((entries,
+    imgObserver) => {
+     entries.forEach(entry => {
+         if (!entry.isIntersecting) {
+             return;
+         } else {
+             preloadImage(entry.target);
+             imgObserver.unobserve(entry.target);
+         }
+     })
+
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
