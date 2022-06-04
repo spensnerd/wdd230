1ace915566a07bcfdb4177312e92f63868e38569
spensnerd <griffinfspencer@gmail.com>
2022-06-04 07:15:07 -0500

    lazy

--- chamber/js/lazyload.js
+++ chamber/js/lazyload.js
@@ -1,61 +1,33 @@
-// let imagesToLoad = document.querySelectorAll('img[data-src]');
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
-// const loadImages = (image) => {
-//     image.setAttribute('src', image.getAttribute('data-src'));
-//     image.onload = () => {
-//         image.removeAttribute('data-src');
-//     };
-// };
-
-// imagesToLoad.forEach((img) => {
-//     loadImages(img);
-// });
-
-// if('IntersectionObserver' in window) {
-//     const observer = new IntersectionObserver((items, observer) => {
-//         items.forEach((item) => {
-//             if(item.isIntersecting) {
-//                 loadImages(item.target);
-//                 observer.unobserve(item.target);
-//             }
-//         });
-//     });
-//     imagesToLoad.forEach((img) => {
-//         observer.observe(img);
-//     });
-// } else {
-//     imagesToLoad.forEach((img) => {
-//         loadImages(img);
-//     });
-// }
-const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-
-    img.src = src;
-}
-
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    // console.log(image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
 };
-const imgObserver = new IntersectionObserver((entries,
-    imgObserver) => {
-     entries.forEach(entry => {
-         if (!entry.isIntersecting) {
-             return;
-         } else {
-             preloadImage(entry.target);
-             imgObserver.unobserve(entry.target);
-         }
-     })
 
-}, imgOptions);
+ const imgOptions = {
+         threshold: 1,
+         rootMargin: "0px 0px 50px 0px"
+     }
 
-images.forEach(image => {
-    imgObserver.observe(image);
-});
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        }, imgOptions);
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
